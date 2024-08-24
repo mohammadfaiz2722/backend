c26f0bdc30d5d08623092eafc565c0b18d531528
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-08-24 15:58:52 +0530

    Updad

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // console.log('Loaded MONGO_URI:', process.env.REACT_APP_API_URL); // Add this line for debugging
-const mongoUri=process.env.MONGO_URI || "mongodb+srv://faiz:faizjarvis@cluster1.lmtoo7r.mongodb.net/FaizBook?retryWrites=true&w=majority&appName=Cluster1"
-
+const mongoUri=process.env.MONGO_URI
 const connectToMongo = () => {
     mongoose.connect(mongoUri, 
         {
