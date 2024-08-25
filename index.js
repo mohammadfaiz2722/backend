af8b9a3efddcc14a02ee7ae384a85749192e0c8e
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-08-25 12:35:22 +0530

    Udpated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path=require('path')
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,16 +11,23 @@ const port = process.env.PORT || 5000;
 connectToMongo();
 
 // Middleware
-app.use(cors()); // Enable CORS
+const corsOptions = {
+  origin: 'https://faizbook-rjwf.onrender.com', // No trailing slash
+};
+
+app.use(cors(corsOptions)); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON request bodies
-app.use(express.static(path.join(__dirname,'./build')))
+app.use(express.static(path.join(__dirname, './build'))); // Serve static files from React app
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
-app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./build/index.html'))
-})
+
+// Fallback for serving React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Backend server is running at http://localhost:${port}`);
